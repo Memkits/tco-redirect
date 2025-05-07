13ec4402495a7ad84a74eeace1545ce585e334e7
tiye <jiyinyiyong@gmail.com>
2025-05-07 13:25:18 +0800

    update to manifest 3 extension

--- extension/background.js
+++ extension/background.js
@@ -1,13 +1,12 @@
-
-chrome.browserAction.onClicked.addListener((tab) => {
-  chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
+chrome.action.onClicked.addListener((tab) => {
+  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
     let tab = tabs[0];
     if (tab != null) {
       let url = tab.pendingUrl || tab.url;
       if (url.startsWith("https://t.co")) {
         let newUrl = `http://tco.tiye.me?url=${url}`;
         console.log("new tab", url, tab.id);
-        chrome.tabs.update(tab.id, {url: newUrl});
+        chrome.tabs.update(tab.id, { url: newUrl });
       } else {
         console.warn("unknown url:", url);
       }
